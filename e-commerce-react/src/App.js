458c7431329231ed6148b71c36c0181f7561a353
theraghulr <rraghul1382@gmail.com>
2025-08-24 14:22:50 +0530

    ft:files

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Nav from "./navigation/Nav";
+
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Nav />
+      <h1>Testing</h1>
     </div>
   );
 }
