ffe6993507479d0eae8a2d32618a4b1f3f025397
theraghulr <rraghul1382@gmail.com>
2025-08-31 16:16:25 +0530

    ft:Siderbar

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Nav from "./navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
 
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <Nav />
       <Recommended />
       <Products />
